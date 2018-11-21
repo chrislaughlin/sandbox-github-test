e423f8f2a5d6636c96ac9b0356e2d7e77263d1f0
Chris Laughlin <bluntm@gmail.com>
2018-11-21 12:58:48 +0000

    Made a change

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 document.getElementById("app").innerHTML = `
 <h1>Github test!</h1>
 <div>
-  Look
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
-  for more info about Parcel.
+  This is a change that I made!!!!!!
 </div>
 `;
